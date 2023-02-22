ec52853afee724a38db469b67b439a753300df63
le-as-a <leasia0817@gmail.com>
2023-02-22 12:16:36 -0600

    header styled

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './header.css';
 
 export default function () {
     return (
         <>
+            <div className='navtitle'>
+                ACNH: Villager Finder
+            </div>
             <div className='nav'>
                 <Link className='navlink' to='/'>Home</Link>
                 <Link className='navlink' to='/catalog'>Catalog</Link>
