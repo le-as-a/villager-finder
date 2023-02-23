922c92c3c0562361e4c655cb45b94d7464d87655
le-as-a <leasia0817@gmail.com>
2023-02-22 23:52:52 -0600

    error page styled

--- src/components/Default/index.js
+++ src/components/Default/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import './default.css';
 
 export default function () {
     return (
-        <div className='def'>
-            Sorry, there was a problem locating this page.<br />
-            <img src={`https://64.media.tumblr.com/3eb6c6a8dae96e98018c70395bc4e3a1/def12ea2a35004b5-50/s400x600/481fcb9fd5e2c61ddb4e3bd9f4246282767c17dc.gif`} />
+        <div className='def-wrap'>
+            <div className='def'>
+                Sorry, there was a problem locating this page.<br />
+                <img src={`https://64.media.tumblr.com/3eb6c6a8dae96e98018c70395bc4e3a1/def12ea2a35004b5-50/s400x600/481fcb9fd5e2c61ddb4e3bd9f4246282767c17dc.gif`} />
+            </div>
         </div>
     )
 }
