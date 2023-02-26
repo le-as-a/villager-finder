68b3a445f772148d1a389f260d22f18628991e24
le-as-a <leasia0817@gmail.com>
2023-02-26 04:24:30 -0600

    frontend thunk actions hypothetically set up

--- src/store/index.js
+++ src/store/index.js
@@ -2,11 +2,13 @@ import { configureStore } from '@reduxjs/toolkit';
 import { catalogSlice } from './catalogSlice';
 import { namesSlice } from './NamesSlice';
 import { villagerSlice } from './villagerSlice';
+import { userSlice } from './userSlice';
 
 export const store = configureStore({
   reducer: {
     names: namesSlice.reducer,
     villager: villagerSlice.reducer,
     catalog: catalogSlice.reducer,
+    user: userSlice.reducer,
   },
 });
--- src/store/userSlice.js
+++ src/store/userSlice.js
@@ -1,1 +1,73 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+
+const initialState = {};
+
+export const authenticate = createAsyncThunk(
+    'user/AUTH',
+    async () => {
+        const res = await fetch('/api/auth');
+
+        if (res.ok) {
+            const data = res.json();
+            if (data.errors) return;
+            return data;
+        }
+    }
+)
+
+export const login = createAsyncThunk(
+    'user/LOGIN',
+    async (email, pw) => {
+        const res = await fetch('/api/auth/login', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                email,
+                password
+            })
+        });
+
+        if (res.ok) {
+            const data = await res.json();
+            return data;
+        } else if (res.status < 500) {
+            const data = await res.json();
+            if (data.errors) {
+                return data.errors;
+            }
+            return data;
+        } else {
+            return ['An error occurred. Please try again.'];
+        }
+    }
+)
+
+export const logout = createAsyncThunk(
+    'user/LOGOUT',
+    async () => {
+        const res = await fetch('/api/auth/logout');
+
+        if (res.ok) {
+            return;
+        }
+    }
+)
+
+export const userSlice = createSlice({
+    name: 'user',
+    initialState,
+    extraReducers: (builder) => {
+        builder.addCase(authenticate.fulfilled, (state, action) => {
+            state = {...action.payload};
+            return state;
+        });
+        builder.addCase(login.fulfilled, (state, action) => {
+            state = {...action.payload};
+            return state;
+        });
+        builder.addCase(logout.fulfilled, () => initialState);
+    },
+    reducers: {}
+});
